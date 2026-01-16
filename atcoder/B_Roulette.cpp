db9191777a56489851dd39f94b182c29d0f1dfc7
meherajmithun <meherajhossainmithun@gmail.com>
2026-01-17 02:04:07 +0600

    added

--- atcoder/B_Roulette.cpp
+++ atcoder/B_Roulette.cpp
@@ -11,21 +11,56 @@
 #define print(x) cout<<x<<'\n'
 using namespace std;
 
+bool cmp(pair<int,int>&a, pair<int,int>&b){
+    if(a.second==b.second) return a.first<b.first;
+    return a.second<b.second;
+}
+
 void solve(){
     int N; cin>>N;
     int n = N;
     vector<vector<int>>cs;
+    vector<int>idx;
     int j=0;
     for(int i=1; i<=N; i++){
         int c; cin>>c;
         vector<int>v(c);
         for(auto &a : v) cin>>a;
         cs.push_back(v); j++;
+        idx.push_back(j);
     }
     int x; cin>>x;
+
+    vector<pair<int,int>>si;
     for(int i=0; i<n; i++){
-        for(auto a : cs[i]) cout<<a<<" "; cout<<nl;
+        for(auto a : cs[i]){
+            if(a==x) si.push_back({idx[i], sz(cs[i])});
+        }
     }
+
+    sort(all(si),cmp);
+
+    vector<int>ans;
+    
+    for(int i=0; i<sz(si)-1; i++){
+        
+        ans.push_back(si[i].first);
+
+        for(int j=i+1; j<sz(si); j++){
+
+            if(si[i].second==si[j].second) ans.push_back(si[j].first);
+
+        }
+        break;
+    }
+
+    if(sz(si)!=0 and sz(ans)==0){
+        ans.push_back(si[0].first);
+    }
+    sort(all(ans));
+    cout<<sz(ans)<<nl;
+    for(auto a : ans) cout<<a<<" ";
+
 }
 
 int32_t main(){
