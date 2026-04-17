232df40dc67a8056c75622474482ae38cf05080e
meherajmithun <meherajhossainmithun@gmail.com>
2026-04-17 08:21:11 +0600

    upsolve

---
+++ codeforces/E_The_67_th_XOR_Problem.cpp
@@ -0,0 +1,33 @@
+// IN THE NAME OF ALLAH
+//#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define int long long
+#define nl '\n'
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+void solve() {
+    int n; cin>>n;
+    vector<int>v(n);
+    for(auto &a : v) cin>>a;
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            ans = max(ans,(v[i]^v[j]));
+        }
+    }
+    cout<<ans<<nl;
+}
+
+int32_t main() {
+    fast
+    int tc = 1;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
