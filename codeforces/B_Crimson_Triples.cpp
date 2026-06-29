52c8ffec497ff5075ae0a9996d51a5c72e623ccb
meherajmithun <meherajhossainmithun@gmail.com>
2026-06-29 08:17:04 +0600

    add upsolve

--- codeforces/B_Crimson_Triples.cpp
+++ codeforces/B_Crimson_Triples.cpp
@@ -9,34 +9,31 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
 #define print(x) cout<<x<<'\n'
-// const int N = 1e6+123;
-// vector<int>cnt(N, 0);
+const int N = 2e5+123;
+vector<int>cnt[N];
 
-// void pre(){
-//     for(int i=1; i<N; i++){
-//         for(int j=i; j<N; j+=i){
-//             cnt[i]++;
-//         }
-//     }
-// }
+void pre(){
+    for(int i=1; i<N; i++){
+        for(int j=i; j<N; j+=i){
+            cnt[j].push_back(i);
+        }
+    }
+}
 
 void solve() {
     int n; cin>>n;
     int ans = 0;
     for(int i=1; i<=n; i++){
-        int cnt = 0;
-        for(int j=i; j<=n; j+=i) cnt++;
-        // cout<<cnt<<" "<<ans<<" ->"<<nl;
-        ans += (1ll*cnt*cnt);
-        // ans += cnt;
-        // cout<<ans<<nl;
+        int c = 0;
+        for(auto a : cnt[i]) c+=(n/a);
+        ans += c;
     }
     print(ans);
 }
 
 int32_t main() {
     fast
-    // pre();
+    pre();
     int tc = 1;
     cin >> tc;
     while (tc--) {
